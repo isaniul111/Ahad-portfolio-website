ff28498e332537aecfb48f4b522213f04dd6c214
Saniul <isaniul999@gmail.com>
2026-01-20 00:12:25 +0600

    updated

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Saniul Islam — Portfolio</title>
+<title>Ahad's — Portfolio</title>
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
